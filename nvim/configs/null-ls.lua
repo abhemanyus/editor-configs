9b04acce4c94d6f297175d821d95f5223732e90e
Abhemanyu Sarin <abhemanyus@gmail.com>
2023-08-23 09:11:03 +0530

    23-08-23

--- nvim/configs/null-ls.lua
+++ nvim/configs/null-ls.lua
@@ -1,26 +1,26 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local b = null_ls.builtins
 
 local sources = {
-  -- webdev stuff
-  b.formatting.prettier, -- so prettier works only on these filetypes
+	-- webdev stuff
+	b.formatting.prettier, -- so prettier works only on these filetypes
 
-  -- Lua
-  b.formatting.stylua,
+	-- Lua
+	b.formatting.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+	-- cpp
+	b.formatting.clang_format,
 
-  -- custom stuff
-  b.diagnostics.shellcheck,
-  b.diagnostics.eslint,
-  b.code_actions.gitsigns,
-  b.formatting.rustfmt,
-  b.completion.spell,
+	-- custom stuff
+	b.diagnostics.shellcheck,
+	b.diagnostics.eslint,
+	b.code_actions.gitsigns,
+	b.formatting.rustfmt,
 }
 
-null_ls.setup {
-  sources = sources,
-  on_attach = require("lspconfig.configs").on_attach,
-}
+null_ls.setup({
+  debug = true,
+	sources = sources,
+	on_attach = require("lspconfig.configs").on_attach,
+})
--- nvim/configs/overrides.lua
+++ nvim/configs/overrides.lua
@@ -1,64 +1,64 @@
 local M = {}
 
 M.treesitter = {
-  ensure_installed = {
-    "lua",
-    "html",
-    "css",
-    "javascript",
-    "typescript",
-    "tsx",
-    "c",
-    "markdown",
-    "markdown_inline",
+	ensure_installed = {
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"c",
+		"markdown",
+		"markdown_inline",
 
-    -- custom stuff
-    "rust",
-    "svelte",
-    "python",
-    "mermaid",
-  },
-  git = {
-    enable = true,
-  },
+		-- custom stuff
+		"rust",
+		"svelte",
+		"python",
+		"mermaid",
+	},
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
-  indent = {
-    enable = true,
-    -- disable = {
-    --   "python"
-    -- },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
 }
 
 M.mason = {
-  ensure_installed = {
-    -- lua stuff
-    "lua-language-server",
-    "stylua",
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
 
-    -- web dev stuff
-    "css-lsp",
-    "html-lsp",
-    "typescript-language-server",
-    "prettier",
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"prettier",
 
-    -- c/cpp stuff
-    "clangd",
-    "clang-format",
+		-- c/cpp stuff
+		"clangd",
+		"clang-format",
 
-    -- custom stuff
-    "rust-analyzer",
-    "shellcheck",
-    "marksman",
-  },
+		-- custom stuff
+		"rust-analyzer",
+		"shellcheck",
+		"marksman",
+	},
 }
 
 return M
