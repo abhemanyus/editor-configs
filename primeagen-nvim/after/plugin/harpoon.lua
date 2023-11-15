882b0fcdcca732f6c23ad46517d93e7461d107ad
Abhemanyu Sarin <abhemanyus@gmail.com>
2023-11-15 10:18:58 +0530

    add lazynvim Wednesday 15 November 2023 10:18:58 AM IST

--- primeagen-nvim/after/plugin/harpoon.lua
+++
@@ -1,8 +0,0 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>hm", mark.add_file)
-vim.keymap.set("n", "<leader>ha", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<leader>hn", ui.nav_next)
-vim.keymap.set("n", "<leader>hp", ui.nav_prev)
